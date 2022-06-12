ceff44fdce517ec3a1176b3a8b0a23d71f35f068
Joy Shaheb <istiaque.joy@northsouth.edu>
2022-06-12 11:05:48 +0600

    work in progress

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -1,5 +1,7 @@
+import React from "react";
+
 const Badge = ({ label, variant, size }) => {
-  return <div classNameName={`badge ${variant} ${size}`}>{label}</div>;
+  return <div className={`badge ${variant} ${size}`}>{label}</div>;
 };
 
 export default Badge;
