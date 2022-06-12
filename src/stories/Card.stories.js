4e242c851130796be0f72e30514269472d7d20d9
Joy Shaheb <istiaque.joy@northsouth.edu>
2022-06-12 11:37:24 +0600

    card widget updated

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "../components/Card/Card";
+import Card from "../widget/Card/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 
